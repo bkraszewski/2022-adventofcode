48e957007d0954ca68261466cd267adda1c6e7e7
bkraszewski <bkraszewski@gmail.com>
2022-12-04 16:09:51 +0100

    bruteforce part 2

--- lib/4th_dec.dart
+++ lib/4th_dec.dart
@@ -50,6 +50,38 @@ void day4partOne() async {
 
 void day4partTwo() async {
   final lines = await readLines('inputs/4_overlaps.txt');
+
+  final score = lines.fold(0, (previousValue, element) {
+    final data = element.split(',');
+    final first = data[0].split('-').map((e) => int.parse(e));
+    final firstAssignment = Assignment(first.first, first.last);
+
+    final second = data[1].split('-').map((e) => int.parse(e));
+    final secondAssignment = Assignment(second.first, second.last);
+
+    //check if first is contained in another
+    //awesome brute force solutions as World Cup game starts in a minute
+
+    for (var a = firstAssignment.start; a <= firstAssignment.end; a++) {
+      if (a >= secondAssignment.start && a <= secondAssignment.end) {
+        return previousValue + 1;
+      } else {
+        continue;
+      }
+    }
+
+    for (var a = secondAssignment.start; a <= secondAssignment.end; a++) {
+      if (a >= firstAssignment.start && a <= firstAssignment.end) {
+        return previousValue + 1;
+      } else {
+        continue;
+      }
+    }
+
+    return previousValue;
+  });
+
+  print('part two score: $score');
 }
 
 class Assignment {
